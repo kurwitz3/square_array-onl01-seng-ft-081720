26817f9622a693432b51f195fd1b8ebaae9882b3
Richard Kurwitz <kurwitz3@aol.com>
2020-08-19 03:39:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
+<<<<<<< HEAD
 array.each_with_index { |a, b| array[b] = a ** 2}
  end
+=======
+array.each do |square|
+ Math.sqrt(square)
+array.push(square)
+ 
+end
+end
+>>>>>>> e3a8c9009453e65dbe9ce95e94ad0d6124a7b109
